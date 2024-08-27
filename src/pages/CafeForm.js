59f6a39b3525b2bd9d07f640d01f01a856e0e76c
kimgura <oli999@naver.com>
2024-08-27 10:33:57 +0900

    Cafe 새글 작성 기능 구현함

--- src/pages/CafeForm.js
+++ src/pages/CafeForm.js
@@ -1,8 +1,9 @@
 // src/pages/CafeForm.js
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button, FloatingLabel, Form } from "react-bootstrap";
 import { initEditor } from "../editor/SmartEditor";
+import axios from "axios";
 
 function CafeForm() {
 
@@ -15,16 +16,20 @@ function CafeForm() {
 		setEditorTool(initEditor("content")); // initEditor() 함수를 호출해야 SmartEditor 가 초기화된다.
     }, [])
 
+    //입력한 내용을 얻어오기 위한 useRef()  
+    const inputTitle=useRef()
+    const inputContent=useRef()
+
     return (
         <>
             <h1>새글 추가 양식입니다</h1>
             <Form>
                 <FloatingLabel label="제목" className="mb-3" controlId="title">
-                    <Form.Control type="text" placeholder="제목 입력..."/>
+                    <Form.Control ref={inputTitle} type="text" placeholder="제목 입력..."/>
                 </FloatingLabel>
                 <Form.Group className="mb-3"  controlId="content">
                     <Form.Label>내용</Form.Label>
-                    <Form.Control as="textarea" rows="10"/>
+                    <Form.Control ref={inputContent} as="textarea" rows="10"/>
                 </Form.Group>
                 <Button type="submit" onClick={(e)=>{
                     //폼 제출 막기(새로고침 방지)
@@ -32,9 +37,16 @@ function CafeForm() {
                     //에디터 tool 을 이용해서 SmartEditor 에 입력한 내용을 textarea 의 value 값으로 변환
                     editorTool.exec();
                     //입력한 제목과 내용을 읽어와서
-
+                    const title=inputTitle.current.value
+                    const content=inputContent.current.value
                     //axios 를 이용해서 api 서버에 전송
-
+                    axios.post("/cafe", {title, content})
+                    .then(res=>{
+                        console.log(res.data)
+                    })
+                    .catch(error=>{
+                        console.log(error)
+                    })
                 }}>저장</Button>
             </Form>
         </>
